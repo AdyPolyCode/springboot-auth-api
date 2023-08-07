e74b6c45d5577ae5aeeb3bbed325027cc8f32281
adypolycode <polcsek.dm90@gmail.com>
2023-08-07 19:58:48 +0200

    init

---
+++ src/main/java/com/poly/schoolDataManager/SchoolDataManagerApplication.java
@@ -0,0 +1,13 @@
+package com.poly.schoolDataManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolDataManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolDataManagerApplication.class, args);
+	}
+
+}
