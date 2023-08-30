adcbf4d6bd48139f3c1a73c4805b6bbc9934614e
adypolycode <polcsek.dm90@gmail.com>
2023-08-30 06:42:56 +0200

    add custom query methods

--- src/main/java/com/poly/schoolDataManager/repositories/UserRepository.java
+++ src/main/java/com/poly/schoolDataManager/repositories/UserRepository.java
@@ -1,6 +1,15 @@
 package com.poly.schoolDataManager.repositories;
 
 import com.poly.schoolDataManager.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface UserRepository extends BaseEntityRepository<User, Long> {
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query(value = "SELECT u FROM User u WHERE u.username = :username")
+    Optional<User> findByUsername(String username);
+
+    @Query(value = "SELECT u FROM User u WHERE u.email = :email")
+    Optional<User> findByEmail(String email);
 }
