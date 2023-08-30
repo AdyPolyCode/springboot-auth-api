e6cd9b21a0bfe77357f44b18b8f291b8667e505e
adypolycode <polcsek.dm90@gmail.com>
2023-08-30 06:42:43 +0200

    add getter/setter

--- src/main/java/com/poly/schoolDataManager/payload/request/UserCreatePayload.java
+++ src/main/java/com/poly/schoolDataManager/payload/request/UserCreatePayload.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class UserCreatePayload {
     @NotEmpty(message = "Username should not be empty")
     @Size(min = 4, max = 12, message = "Username length must be between 4 and 12 included")
@@ -18,4 +18,28 @@ public class UserCreatePayload {
     @NotEmpty(message = "Password should not be empty")
     @Size(min = 8, max = 16, message = "Password length must be between 8 and 16 included")
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/com/poly/schoolDataManager/payload/request/UserUpdatePayload.java
+++ src/main/java/com/poly/schoolDataManager/payload/request/UserUpdatePayload.java
@@ -17,4 +17,28 @@ public class UserUpdatePayload {
 
     @Size(min = 8, max = 16, message = "Password length must be between 8 and 16 included")
     private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
