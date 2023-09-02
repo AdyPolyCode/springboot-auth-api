7475ada4001637322ce0a59ffc66f0f6acd8d4d1
adypolycode <polcsek.dm90@gmail.com>
2023-09-02 20:06:57 +0200

    add more field getters/setters

--- src/main/java/com/poly/schoolDataManager/payload/response/UserPayload.java
+++ src/main/java/com/poly/schoolDataManager/payload/response/UserPayload.java
@@ -1,9 +1,27 @@
 package com.poly.schoolDataManager.payload.response;
 
+import com.poly.schoolDataManager.entities.UserRole;
+
+import java.util.Set;
+
 public class UserPayload {
+    private Long id;
+
     private String username;
 
     private String email;
+
+    private String password;
+
+    private Set<UserRole> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -19,5 +37,21 @@ public class UserPayload {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<UserRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<UserRole> roles) {
+        this.roles = roles;
     }
 }
