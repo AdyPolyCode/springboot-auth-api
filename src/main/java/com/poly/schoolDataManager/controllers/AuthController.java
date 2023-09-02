f18a53107ffaa64dac1b27fd3501a3543e8cd081
adypolycode <polcsek.dm90@gmail.com>
2023-09-02 20:06:03 +0200

    refactor with independent service

--- src/main/java/com/poly/schoolDataManager/controllers/AuthController.java
+++ src/main/java/com/poly/schoolDataManager/controllers/AuthController.java
@@ -1,26 +1,37 @@
 package com.poly.schoolDataManager.controllers;
 
-import com.poly.schoolDataManager.entities.User;
-import com.poly.schoolDataManager.services.UserService;
+import com.poly.schoolDataManager.payload.request.UserCreatePayload;
+import com.poly.schoolDataManager.payload.request.UserLoginPayload;
+import com.poly.schoolDataManager.services.AuthService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
-    private UserService service;
+    private AuthService service;
 
     @Autowired
-    public AuthController(UserService service) {
+    public AuthController(AuthService service) {
         this.service = service;
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login() { return null; }
+    public ResponseEntity<String> login(@RequestBody @Valid UserLoginPayload payload) {
+        String token = service.login(payload.getUsername(), payload.getPassword());
+
+        return ResponseEntity.ok(token);
+    }
 
     @PostMapping("/register")
-    public ResponseEntity<User> register() { return null; }
+    public ResponseEntity<String> register(@RequestBody @Valid UserCreatePayload payload) {
+        String token = service.register(payload);
+
+        return ResponseEntity.ok(token);
+    }
 }
